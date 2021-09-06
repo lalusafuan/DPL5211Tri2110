60dfec8a23430d8f600dc83ff14b5027caeef98e
lalusafuan <90165943+lalusafuan@users.noreply.github.com>
2021-09-06 10:53:28 +0800

    Add files via upload

---
+++ Lab 1.1.py
@@ -0,0 +1,7 @@
+# Student ID: 1201201699
+# Student Name: Lalu Muhammad Safuan Bin Maazar
+
+print("I like Python")
+
+person = input("Please enter your name: ")
+print("Hello ", person)
---
+++ Lab 1.2.py
@@ -0,0 +1,8 @@
+#Lab 1.2 get input two numbers and display the sum of the two numbers
+
+number1 = int(input("Enter first number : "))
+number2 = int(input("Enter second number : "))
+
+sum = number1 + number2
+
+print("The Sum of {} and {} is {}".format(number1,number2,sum))
